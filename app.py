a6f50d54d907173c67fd2a257189e51a4da6732c
cngo1 <54957080+cngo1@users.noreply.github.com>
2019-11-14 20:05:39 -0500

    Help

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 """
-Simple "Hello, World" application using Flask
+Application that helps find the nearest station and whether or not it is wheel chair accessible.
 """
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/mbta_helper/", methods = ["GET", "POST"])
+def location():
+    if request.method == "POST":
+        place_name = str(request.form["location"])
+        station_name, wheelchair_accessible = find_stop_near(place_name)
+        value = ""
+        if station_place:
+            if wheelchair == 2:
+                value = 'Inacessible'
+            elif wheelchair == 1:
+                value = "Accessible"
+            else:
+                value = "No information"
+            return render_template("return_message.html", place_name = place_name, station_name=station_name, wheelchair_accessible=wheelchair_accessible)
+        else:
+            return render_template("mbta_helper.html", error=True)
+        return render_template("mbta_helper.html", error=None)
--- mbta_helper.py
+++ mbta_helper.py
@@ -3,18 +3,26 @@ MAPQUEST_BASE_URL = "http://open.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
+MAPQUEST_API_KEY = "yVdtBiZA6oLGT8mE9AkXYYszWmPsx1rD"
+MBTA_API_KEY = "22c19fd52b5e4c2ea304b61e8bbee70d"
 
 
 # A little bit of scaffolding if you want to use it
+
+import json
+import urllib.request
+from pprint import pprint
+
 
 def get_json(url):
     """
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    return response_data
 
 
 def get_lat_long(place_name):
@@ -24,7 +32,13 @@ def get_lat_long(place_name):
     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
     for Mapquest Geocoding  API URL formatting requirements.
     """
-    pass
+    location = place_name.replace(' ', '%20')
+    MAPQUEST = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={location}'
+    response = get_json(MAPQUEST)
+    latitude_longitude = response['results'][0]['locations'][0]['displayLatLng']
+    latitude = latitude_longitude['lat']
+    longitude = latitude_longitude['lng']
+    return (latitude, longitude)
 
 
 def get_nearest_station(latitude, longitude):
@@ -34,21 +48,38 @@ def get_nearest_station(latitude, longitude):
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
     """
-    pass
+    MBTA = f'{MBTA_BASE_URL}?api_key={MBTA_API_KEY}&filter[latitude]={latitude}&filter[longitude]={longitude}&sort=distance'
+    response2 = get_json(MBTA)
+    for *station_name, wheelchair_accessible in response2:
+        station_name = response2['data'][0]['attributes']['name']
+        wheelchair_accessible = response2['data'][0]['attributes']['wheelchair_boarding']
+        return (station_name, wheelchair_accessible)
+    return None, None
 
 
 def find_stop_near(place_name):
     """
     Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
     """
-    pass
+    latitude, longitude = get_lat_long(place_name)
+    station, wheelchair = get_nearest_station(latitude, longitude)
+    if wheelchair == 2:
+        return "Inaccessible"
+    elif wheelchair == 1:
+        return "Accessible"
+    else:
+        return "No information"
+    return station, wheelchair
+    
 
 
 def main():
     """
     You can all the functions here
     """
-    pass
+    #print(get_lat_long('Faneuil Hall Marketplace'))
+    #print(get_nearest_station(42.36048, -71.05414))
+    # print(find_stop_near('Prudential Center'))
 
 
 if __name__ == '__main__':
